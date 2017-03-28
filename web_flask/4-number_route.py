d8212bd1cca0a7fb3decec9c954277d2438d4604
Walton Lee <mr.waltonlee@me.com>
2017-03-28 21:05:01 +0000

    displaying a message if n is a number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+from flask import Flask, url_for
+app = Flask(__name__)
+
+@app.route('/')
+def display_Hello():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb')
+def display_HBNB():
+    return 'HBNB'
+
+@app.route('/c/(<text>)')
+def replace_c(text):
+    if "_" in text:
+        text = text.replace("_", " ")
+        return 'C %s' % text
+    else:
+        return 'C %s' % text
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>')
+def replace_py(text="is cool"):
+    if "_" in text:
+        text = text.replace("_", " ")
+        return 'Python %s' % text
+    else:
+        return 'Python %s' % text
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def only_num(n):
+    return "%d is a number" % n
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
