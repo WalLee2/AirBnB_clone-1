660e1b8e8f54062ec557a458bc1e6a9f8f0251a7
Walton Lee <mr.waltonlee@me.com>
2017-03-28 18:47:48 +0000

    Starts a flask web application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def display():
+    return 'Hello HBNB!'
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
