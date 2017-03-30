fa02f869933edf0655ae1f23943be8222086b00e
Walton Lee <mr.waltonlee@me.com>
2017-03-30 00:39:58 +0000

    fixing pep8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -11,10 +11,12 @@ def show_state():
     my_list = storage.all("State").values()
     return render_template('7-states_list.html', my_list=my_list)
 
+
 @app.route('/cities_by_states')
 def cities_by_states_html():
     my_list = storage.all("State").values()
     return render_template('8-cities_by_states.html', my_list=my_list)
+
 
 @app.teardown_appcontext
 def teardown(self):
